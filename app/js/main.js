8560e1cffcb2ca979c12561cda159d36e51ee49d
“VitaliyNickishin” <nickishin34@gmail.com>
2019-04-01 14:18:24 +0300

    file structuring and continue project

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,10 @@
-/*
+/**/
 $('.navbar-toggler').on('click', function(e) {
  e.preventDefault();//для отмены действия при нажатии кнопки
  $(this).toggleClass('navbar-toggler_active');//находим.navbar-toggler и создаем класс navbar-toggler_active для переварачивания полосок
  
 });
-*/
+
 /*
 windows.onload = function(){
 
