8105dc827e3aa32e5b7775cadaf5883d935ab92c
“VitaliyNickishin” <nickishin34@gmail.com>
2019-04-10 18:01:35 +0300

    work with animation

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,20 @@
-/**/
+/*Кнопка меню*/
 $('.navbar-toggler').on('click', function(e) {
  e.preventDefault();//для отмены действия при нажатии кнопки
  $(this).toggleClass('navbar-toggler_active');//находим.navbar-toggler и создаем класс navbar-toggler_active для переварачивания полосок
  
 });
-
+/*
+jQuery(function($){
+  $("#vc_pop").hide();
+  $("#viewport").viewportChecker({
+  callbackFunction:function(){
+  $("#vc_pop").show();
+  $("#viewport").show();
+  }
+  });
+  });
+  */
 /*
 windows.onload = function(){
 
